07f796d55be95a1297d9ef1e664366eab55b4575
Parsa Barooj <p.barooj7@gmail.com>
2026-06-27 18:39:58 +0330

    create gameobject & gamesetting

---
+++ SpaceShooter/GameCore/GameObject.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceShooter.GameCore
+{
+    public abstract class GameObject
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public int Width { get; protected set; }
+        public int Height { get; protected set; }
+        public int HP { get; set; }
+        public int MaxHP { get; protected set; }
+        public bool IsAlive => HP > 0;
+
+        public Rectangle Bounds => new Rectangle((int)X, (int)Y, Width, Height);
+
+        protected GameObject(float x, float y, int width, int height, int hp)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+            HP = hp;
+            MaxHP = hp;
+        }
+
+        public abstract void Update(float deltaTime);
+        public abstract void Draw(Graphics g);
+
+        public bool CollidesWith(GameObject other)
+        {
+            return Bounds.IntersectsWith(other.Bounds);
+        }
+
+        public void TakeDamage(int amount)
+        {
+            HP -= amount;
+            if (HP < 0) HP = 0;
+        }
+    }
+}
---
+++ SpaceShooter/GameCore/GameSetting.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceShooter.GameCore
+{
+    public static class GameSettings
+    {
+        public const int ScreenWidth = 600;
+        public const int ScreenHeight = 750;
+
+        public const int PlayerStartHP = 100;
+        public const int PlayerStartLives = 3;
+        public const int PlayerSpeed = 5;
+        public const int BulletSpeed = 10;
+        public const int FireCooldownMs = 220;
+
+        public const int TotalWaves = 10;
+        public const int TimerIntervalMs = 20;
+
+        public static float EnemySpeedScale(int wave) => 1f + 0.1f * wave;
+        public static int EnemyHPBonus(int wave) => 2 * wave;
+    }
+}
